7b8d1cafc1850aa0e88011f5870f8c58ddd0a315
李沈弘 <mjdfx120312@163.com>
2022-10-17 09:01:25 +0800

    2022-10-17

--- src/main/java/com/SEC/SEC_MealTicketApply_Backend/Dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.SEC.SEC_MealTicketApply_Backend.Dao;
-
-import com.SEC.SEC_MealTicketApply_Backend.Domain.User;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao extends BaseMapper<User> {
-}
